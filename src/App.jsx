7a221f0f74b1544e187a8af420f5a335313fc5e5
Dejan <dejan.mihajlov@hotmail.com>
2022-09-21 20:51:32 +0200

    Implementing API call

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [cordinates, setCordinates] = useState({
-    lat: "41.99646",
-    lon: "21.43141",
-  });
+  const [cityName, setCityName] = useState("Skopje");
   const [data, setData] = useState({});
+  const [cityData, setCityData] = useState('')
 
   const doFetch = useCallback(async () => {
-    const { lat, lon } = cordinates;
+    console.log('here', cityName)
     await axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${"0293d762927c3ac3a2d4dc21acb9f6c2"}`
+        // `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${"0293d762927c3ac3a2d4dc21acb9f6c2"}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${"0293d762927c3ac3a2d4dc21acb9f6c2"}`
       )
       .then((res) => {
         setData(res.data);
@@ -20,23 +19,27 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-  }, [cordinates]);
+  }, [cityName]);
 
   useEffect(() => {
     doFetch();
   }, [doFetch]);
 
-  useEffect(() => {
-    doFetch();
-  }, [doFetch, cordinates.lat, cordinates.lon]);
+  const handleChange = (e) => {
+    setCityData(e)
+  }
+
+  const handleClick = () => {
+    setCityName(cityData)
+  }
 
   if (data && data.weather) {
-    console.log(data);
 
     return (
       <div className="app">
         <div className="search">
-          <input placeholder="Enter location" type="text" />
+          <input placeholder="Enter location" type="text" value={cityData} onChange={(e)=> handleChange(e.target.value)} />
+          <button onClick={()=>handleClick()} style={{width: '100px', height: '30px'}}>Search</button>
         </div>
         <div className="container">
           <div className="top">
@@ -44,7 +47,9 @@ function App() {
               <p>{data.name}</p>
             </div>
             <div className="temp bold">
-              {data.main.temp ? <h1>{data.main.temp}&#176;</h1> : null}
+              {data.main.temp ? (
+                <h1>{Math.floor(data.main.temp)}&#176;</h1>
+              ) : null}
             </div>
             <div className="description bold">
               {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -52,7 +57,9 @@ function App() {
           </div>
           <div className="bottom">
             <div className="feels bold">
-              {data.main ? <p>{data.main.feels_like}&#176;</p> : null}
+              {data.main ? (
+                <p>{Math.floor(data.main.feels_like)}&#176;</p>
+              ) : null}
               <p>Feels like</p>
             </div>
             <div className="humidity bold">
@@ -60,7 +67,7 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind bold">
-              {data.wind ? <p>{data.wind.speed}km</p> : null}
+              {data.wind ? <p>{Math.floor(data.wind.speed)}km</p> : null}
               <p>Wind</p>
             </div>
           </div>
